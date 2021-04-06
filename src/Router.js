0ccd9dabe0c46e7e435b2c8b66944d6f3cb22c44
heunyam <tpwns072@gmail.com>
2021-04-06 19:06:13 +0900

    feat: routing에 /code 추가

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Login from './components/login/Login';
+import LoginPage from './components/login/Login';
 import GetAccessToken from './components/login/OAuth';
+import SnippetPage from './components/snippet/snippet'
 
 
 const App = () => {
   return (
     <div>
-      <Route path="/login" component={Login} />
+      <Route path="/login" component={LoginPage} />
       <Route path="/github/OAuth" component={GetAccessToken}/>
+      <Route path="/code" component={SnippetPage}/>    
     </div>
   );
 };
