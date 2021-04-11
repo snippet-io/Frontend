2e91dc435b3ae1130c799877a69847f1a4d76467
heunyam <tpwns072@gmail.com>
2021-04-11 19:54:48 +0900

    fix: Store access token logic

--- src/components/login/OAuth.js
+++ src/components/login/OAuth.js
@@ -11,12 +11,12 @@ const GetAccessToken = ({ history, location }) => {
 
       try {
         // 이 부분은 express에 요청하여 JWT 토큰을 발급합니다.
-        const { access_token } = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/github/accesstoken`, {
+        const res = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/github/accesstoken`, {
           code,
         });
 
         // 유저 JWT 토큰을 저장합니다.
-        localStorage.setItem('accessToken', access_token);
+        localStorage.setItem('accessToken', res.data.access_token);
         history.push('/code'); // 로그인이 완료되면 보여줄 페이지✔
       } catch (error) {
         history.push('/error');
