38a4e650510d09e70cc82028f75fe261a8e217cf
heunyam <tpwns072@gmail.com>
2021-04-19 08:53:29 +0900

    feat: "/" path move to Snippet list page

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LoginPage from './components/login/Login';
 import GetAccessToken from './components/login/OAuth';
-import SnippetPage from './components/snippet/snippet'
+import SnippetPage from './components/snippet/Snippet'
+import SnippetsPage from './components/snippets/Snippets';
 
 
 const App = () => {
   return (
     <div>
+      <Route exact path="/" component={SnippetsPage} />
       <Route path="/login" component={LoginPage} />
       <Route path="/github/OAuth" component={GetAccessToken}/>
       <Route path="/code" component={SnippetPage}/>
