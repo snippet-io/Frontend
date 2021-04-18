172cbb0db8168a11d858b1b5e5274b856e82530d
heunyam <tpwns072@gmail.com>
2021-04-19 08:51:50 +0900

    style: flex

--- src/components/style.js
+++ src/components/style.js
@@ -5,4 +5,7 @@ export const BaseBackground = styled.div`
     background: linear-gradient(#000d18, #072136, #0f2b43, #2b3d4d);
     width: 100%;
     height: 100%;
+
+    display: flex;
+    justify-content: center;
 `;
