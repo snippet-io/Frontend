a354cf80762649bb92fee780d9940949192f0a77
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-03 11:52:13 +0900

    feat: Compose approximate UI about snippet header

---
+++ src/app/snippetDetail/components/snippetDetail.js
@@ -0,0 +1,13 @@
+import Header from "app/header/components/header";
+import SnippetHeader from "./snippetHeader";
+
+
+const SnippetDetail = () => {
+    return (
+    <>
+        <Header />
+        <SnippetHeader />
+    </>);
+};
+
+export default SnippetDetail;
