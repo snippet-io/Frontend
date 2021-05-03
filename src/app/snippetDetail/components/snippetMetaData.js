17f47462d051170d96017222d1b3e70875a0f3db
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-03 11:49:14 +0900

    feat: Add SnippetMetaData component

---
+++ src/app/snippetDetail/components/snippetMetaData.js
@@ -0,0 +1,14 @@
+import Author from "./author";
+import CreatedTime from "./createdTime";
+
+const SnippetMetaData = () => {
+    return (
+        <div>
+            <Author />
+            -
+            <CreatedTime />
+        </div>
+    );
+};
+
+export default SnippetMetaData;
