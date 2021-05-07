e4007450166536eed9729ac7829d460320aacf0c
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 14:43:38 +0900

    style: Apply prettier foamatter

--- src/app/snippetDetail/components/SnippetDetailLink.js
+++ src/app/snippetDetail/components/SnippetDetailLink.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import * as S from "./style";
 
-
-const SnippetDetailLink = ({to, children}) => {
-    return (
-        <Link to={to}><S.SnippetDetailLink>{children}</S.SnippetDetailLink></Link>
-    );
+const SnippetDetailLink = ({ to, children }) => {
+  return (
+    <Link to={to}>
+      <S.SnippetDetailLink>{children}</S.SnippetDetailLink>
+    </Link>
+  );
 };
 
 export default SnippetDetailLink;
--- src/app/snippetDetail/components/snippetDescription.js
+++ src/app/snippetDetail/components/snippetDescription.js
@@ -1,10 +1,7 @@
 import * as S from "./style";
 
-const SnippetDescription = ({description}) => {
-
-    return (
-        <S.SnippetDescription>{description}</S.SnippetDescription>
-    );
+const SnippetDescription = ({ description }) => {
+  return <S.SnippetDescription>{description}</S.SnippetDescription>;
 };
 
 export default SnippetDescription;
