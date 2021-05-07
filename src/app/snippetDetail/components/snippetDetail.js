3d5c96cfe4bcb04fb1bcd687bd821c69bb600e89
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 11:50:25 +0900

    fix: Remove codes for temporary test

--- src/app/snippetDetail/components/snippetDetail.js
+++ src/app/snippetDetail/components/snippetDetail.js
@@ -3,25 +3,10 @@ import Header from "app/header/components/header";
 import SnippetHeader from "./snippetHeader";
 import * as S from "./style";
 import SnippetCode from "./snippetCode";
-import languages from "lib/languages";
-import { Link } from "react-router-dom";
 import SnippetDetailLink from "./SnippetDetailLink";
 
 const SnippetDetail = ({snippet, isUserAuthor, urlGoingEdit, urlGoingToBackTheList}) => {
 
-    snippet = {
-        title: "Title",
-        language: languages["javascript"],
-        author: "손정우",
-        createdDateTime: new Date(),
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-                      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                      book`,
-        code: `function sum(a, b) {
-            return a + b;
-        }`
-    };
-    isUserAuthor = true;
     return (
     <>
         <Header />
