40516b6072ec3791de6cae41cf566fddee95ed6f
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 15:12:13 +0900

    feat: Add calculateCreatedTimestamp

--- src/utils/createdTime.js
+++ src/utils/createdTime.js
@@ -1,6 +1,6 @@
-export const calculateCreatedTimeStamp = (createdTimeStamp) => {
+export const calculateCreatedTimestamp = (createdTimestamp) => {
   const today = new Date();
-  const diffMS = today.getTime() - createdTimeStamp.getTime();
+  const diffMS = today.getTime() - createdTimestamp.getTime();
   const diffMunitues = parseInt(diffMS / (1000 * 60));
   const diffHours = parseInt(diffMunitues / 60);
 
@@ -10,9 +10,9 @@ export const calculateCreatedTimeStamp = (createdTimeStamp) => {
   } else if (diffHours < 24) {
     stringifiedCreatedTime = `${diffHours}시간 전`;
   } else {
-    stringifiedCreatedTime = `${createdTimeStamp.getFullYear()}년 ${
-      createdTimeStamp.getMonth() + 1
-    }월 ${createdTimeStamp.getDate()}일`;
+    stringifiedCreatedTime = `${createdTimestamp.getFullYear()}년 ${
+      createdTimestamp.getMonth() + 1
+    }월 ${createdTimestamp.getDate()}일`;
   }
   return stringifiedCreatedTime;
 };
