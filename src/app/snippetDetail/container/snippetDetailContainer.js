3e02a3bf78ea6e01713190034aeb192a68264c48
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 20:04:10 +0900

    feat: Use effect

--- src/app/snippetDetail/container/snippetDetailContainer.js
+++ src/app/snippetDetail/container/snippetDetailContainer.js
@@ -2,29 +2,28 @@ import SnippetDetail from "../components/snippetDetail";
 import languages from "lib/languages";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {} from "lib/api";
+import { getCodeAPI, getUserAPI } from "lib/api";
 
 const SnippetDetailContainer = () => {
   const { snippetId } = useParams();
 
-  // const [snippet, setSnippet] = useState();
-  // useEffect(() => {
-
-  // });
-
-  // console.log(snippetId);
-  const snippet = {
-    title: "Title",
-    language: languages["javascript"],
-    author: "손정우",
-    createdDateTime: new Date(),
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-                      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                      book`,
-    code: `function sum(a, b) {
-            return a + b;
-        }`,
-  };
+  const [snippet, setSnippet] = useState();
+  useEffect(() => {
+    (async () => {
+      const snippetData = await getCodeAPI(snippetId);
+      const authorData = await getUserAPI(snippetData.data.author);
+      const snippet = {
+        id: snippetData.data.id,
+        title: snippetData.data.title,
+        code: snippetData.data.content,
+        author: authorData.data.name,
+        language: languages[snippetData.data.language],
+        desciption: snippetData.data.description,
+        createdDateTime: new Date(snippetData.data.created_datetime),
+      };
+      setSnippet(snippet);
+    })();
+  }, [snippetId]);
   const isUserAuthor = true;
   return (
     <SnippetDetail
