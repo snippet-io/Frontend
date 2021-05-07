79696c66b7e7fac5092a80aceefb7a4ba1ba7980
heunyam <tpwns072@gmail.com>
2021-05-07 13:53:34 +0900

    refactor: 하이라이팅 언어 import 분리

--- src/app/createSnippet/components/createSnippet.js
+++ src/app/createSnippet/components/createSnippet.js
@@ -3,12 +3,7 @@ import Editor from "react-simple-code-editor";
 import Header from "app/header/container/headerContainer";
 import * as S from "./style";
 import { postCreateSnippetAPI } from "lib/api";
-
 import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-java";
-import "prismjs/themes/prism.css";
 
 const CreateSnippet = () => {
   const [inputs, setInputs] = useState({
