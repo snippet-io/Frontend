2db9ebcbfe934d3d42dc2409e3d34ac60e73045a
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 20:11:55 +0900

    refactor: Use util function

--- src/app/snippetDetail/components/createdTime.js
+++ src/app/snippetDetail/components/createdTime.js
@@ -1,21 +1,8 @@
 import * as S from "./style";
+import { calculateCreatedTimestamp } from "utils/createdTime";
 
 const CreatedTime = ({ createdTime }) => {
-  const today = new Date();
-  const diffMS = today.getTime() - createdTime.getTime();
-  const diffMunitues = parseInt(diffMS / (1000 * 60));
-  const diffHours = parseInt(diffMunitues / 60);
-
-  let stringifiedCreatedTime;
-  if (diffHours < 1) {
-    stringifiedCreatedTime = `${diffMunitues}분 전`;
-  } else if (diffHours < 24) {
-    stringifiedCreatedTime = `${diffHours}시간 전`;
-  } else {
-    stringifiedCreatedTime = `${createdTime.getFullYear()} / ${
-      createdTime.getMonth() + 1
-    } / ${createdTime.getDate()}`;
-  }
+  const stringifiedCreatedTime = calculateCreatedTimestamp(createdTime);
 
   return <S.CreatedTime>{stringifiedCreatedTime}</S.CreatedTime>;
 };
