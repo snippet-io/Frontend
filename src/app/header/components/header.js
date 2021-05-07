07e5bbca8d1be87d5163f466342a5c7e3d3ba87c
heunyam <tpwns072@gmail.com>
2021-05-07 13:36:16 +0900

    feat: header link

--- src/app/header/components/header.js
+++ src/app/header/components/header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import * as S from "./style";
 import logoImg from "static/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <S.HeaderTemplate>
-      <S.Logo src={logoImg} />
+      <Link to="/">
+        <S.Logo src={logoImg} />
+      </Link>
       <S.HeaderUserSection>
         <S.HeaderLink to="/new">new</S.HeaderLink>
         <S.HeaderLink to="/login">login</S.HeaderLink>
