f832214c330ab9f727d538d013bb3561415b29cf
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-11 16:16:56 +0900

    feat: Inject state to EditSinppet component

--- src/app/snippet/containter/editSnippetContainer.js
+++ src/app/snippet/containter/editSnippetContainer.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import EditSnippet from "../components/editSnippet/editSnippet";
+
 const EditSnippetContainer = () => {
-  return <EditSnippet></EditSnippet>;
+  const [snippet, setSnippet] = useState({
+    title: "title",
+    code: "code",
+    language: "python",
+    description: "description",
+  });
+  const onChangeInput = (e) => {
+    const { value, name } = e.target;
+    setSnippet({
+      ...snippet,
+      [name]: value,
+    });
+  };
+  const snippetState = {
+    ...snippet,
+    setSnippet,
+  };
+  return (
+    <EditSnippet
+      snippetState={snippetState}
+      onChangeInput={onChangeInput}
+    ></EditSnippet>
+  );
 };
 
 export default EditSnippetContainer;
