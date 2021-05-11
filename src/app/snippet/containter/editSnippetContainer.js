6945084af1befcb486874771823d5572165b8183
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-11 15:39:31 +0900

    feat: Route EditSnippetContainer

---
+++ src/app/snippet/containter/editSnippetContainer.js
@@ -0,0 +1,6 @@
+import EditSnippet from "../components/editSnippet/editSnippet";
+const EditSnippetContainer = () => {
+  return <EditSnippet></EditSnippet>;
+};
+
+export default EditSnippetContainer;
