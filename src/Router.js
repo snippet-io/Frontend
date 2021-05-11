35cf2328f3c8a89d349e5d899f2bc06051faa9ae
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-11 20:50:39 +0900

    feat: Route not found container

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import OAuthContainer from "app/login/container/oauthContainer";
 import SnippetListContainer from "app/snippetList/container/snippetListContainer";
 import CreateSnippetContainer from "app/snippet/containter/createSnippetContainer";
 import SnippetDetailContainer from "app/snippetDetail/container/snippetDetailContainer";
+import NotFoundContainer from "app/error/notFound/container/notFoundContainer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/github/OAuth" component={OAuthContainer} />
       <Route exact path="/" component={SnippetListContainer} />
       <Route path="/codes/:snippetId" component={SnippetDetailContainer} />
+      <Route path="/404" component={NotFoundContainer} />
     </div>
   );
 };
