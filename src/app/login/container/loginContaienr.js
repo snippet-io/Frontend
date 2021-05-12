506f4cd3b90855386a705e5af8b6299892614c94
heunyam <tpwns072@gmail.com>
2021-05-12 12:09:20 +0900

    fix: login

--- src/app/login/container/loginContaienr.js
+++ src/app/login/container/loginContaienr.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
+
 import Login from "../components/login";
+import { getOAuthUrlAPI } from "lib/api";
 
 const LoginContainer = () => {
-  const gotoURL = (url) => {
-    window.location.href = url;
+  const [redirectUrl, setRedirectUrl] = useState("");
+
+  useEffect(() => {
+    getOAuthUrlAPI().then((res) => {
+      setRedirectUrl(res.data.redirect_url);
+    });
+  }, []);
+
+  const githubOAuthLogin = () => {
+    window.location.href = redirectUrl;
   };
 
-  return <Login gotoURL={gotoURL} />;
+  return <Login githubOAuthLogin={githubOAuthLogin} />;
 };
 
 export default LoginContainer;
