f515c480c0687f4555962d7e208ff524fed4605e
heunyam <tpwns072@gmail.com>
2021-05-16 03:52:52 +0900

    feat: snippetItem UI

--- src/app/snippetList/components/snippetItem/snippetItem.js
+++ src/app/snippetList/components/snippetItem/snippetItem.js
@@ -10,12 +10,31 @@ const SnippetItem = ({
 }) => {
   return (
     <>
-      <h1>{title}</h1>
+      <S.SnippetItemWarpper>
+        <S.FrontWarpper>
+          <S.HeaderWarpper>
+            <S.ProfileImg />
+            <S.HeaderContentWarpper>
+              <S.UserName>이름이에요</S.UserName>
+              <S.Title>
+                제목을 달아요<S.Language>JS</S.Language>
+              </S.Title>
+            </S.HeaderContentWarpper>
+          </S.HeaderWarpper>
+          <S.Description>설명을 달아요</S.Description>
+        </S.FrontWarpper>
+
+        <S.FooterWarpper>
+          <S.Star />
+          <S.Datetime>Created 10 minutes ago</S.Datetime>
+        </S.FooterWarpper>
+      </S.SnippetItemWarpper>
+      {/* <h1>{title}</h1>
       <p>{content}</p>
       <p>{description}</p>
       <p>{created_datetime}</p>
 
-      <div>{author}</div>
+      <div>{author}</div> */}
     </>
   );
 };
--- src/app/snippetList/components/snippetItem/style.js
+++ src/app/snippetList/components/snippetItem/style.js
@@ -1,5 +1,85 @@
 import styled from "styled-components";
 
-export const Title = styled.p`
-  font-size: "22px";
+export const SnippetItemWarpper = styled.article`
+  display: flex;
+  flex-direction: row;
+
+  padding: 1rem;
+  border-radius: 0.15rem;
+  border: 1px solid #000000;
+`;
+export const FrontWarpper = styled.div``;
+
+export const HeaderWarpper = styled.div`
+  display: flex;
+`;
+
+export const HeaderContentWarpper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 0 0 0.4rem;
+`;
+
+export const Title = styled.h2`
+  display: flex;
+  align-items: center;
+
+  height: 80%;
+  margin: 0 0 0 0rem;
+  font-size: 1.256rem;
+`;
+
+export const UserName = styled.p`
+  margin: 0 0 0 0rem;
+`;
+
+export const ProfileImg = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  border: 1px solid #000000;
+  border-radius: 100rem;
+`;
+
+export const Description = styled.p`
+  color: #7e857e;
+
+  margin: 1rem 0 0 3rem;
+`;
+
+export const Language = styled.p`
+  font-size: 1rem;
+  font-weight: lighter;
+  color: #ffffff;
+
+  padding: 0 0.8rem 0 0.8rem;
+  margin: 0 0 0 0.5rem;
+
+  border: 1px solid #000000;
+  border-radius: 100rem;
+  background-color: #000000;
+`;
+
+export const FooterWarpper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+`;
+
+export const Datetime = styled.p`
+  display: flex;
+
+  margin: 0 0 0 0;
+  margin-top: auto;
+`;
+
+export const StarCount = styled.p``;
+
+export const Star = styled.img`
+  display: flex;
+  align-self: flex-end;
+
+  width: 1.2rem;
+  height: 1.2rem;
+  border: 1px solid black;
 `;
