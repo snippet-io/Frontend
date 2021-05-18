98c314c5cad6afbe1cbb676f9d5f0414932d54ee
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-18 11:12:38 +0900

    fit: Change prism initing module

--- src/lib/languages.js
+++ src/lib/languages.js
@@ -1,4 +1,4 @@
-import "prismjs";
+import "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
