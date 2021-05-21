34c0f14b1b3229577f1c1ff8c698c1ce82e22bff
heunyam <tpwns072@gmail.com>
2021-05-21 22:27:28 +0900

    feat: sort bar에 선택 된 상태 추가

--- src/app/snippetList/container/sortBarContainer.js
+++ src/app/snippetList/container/sortBarContainer.js
@@ -1,8 +1,16 @@
 import SortBar from "../components/sortBar/sortBar";
 import languageList from "lib/languages";
+import { useState } from "react";
 
 const SortBarContainer = () => {
-  return <SortBar languages={languageList}></SortBar>;
+  const [selectedLanguage, setSelectedLanguage] = useState("All");
+  return (
+    <SortBar
+      languages={languageList}
+      selected={selectedLanguage}
+      setSelected={setSelectedLanguage}
+    ></SortBar>
+  );
 };
 
 export default SortBarContainer;
