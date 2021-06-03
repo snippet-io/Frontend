49206faa8fd2b028d0b3a49efd65040106dcd39d
heunyam <tpwns072@gmail.com>
2021-06-03 19:12:46 +0900

    feat: login 시 프로필로

--- src/app/header/container/headerContainer.js
+++ src/app/header/container/headerContainer.js
@@ -1,9 +1,25 @@
+import { getUserAPI } from "lib/api";
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 
 const HeaderContainer = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    (async () => {
+      console.log(localStorage.getItem("id"));
+      if (localStorage.getItem("id")) {
+        const userData = await getUserAPI(localStorage.getItem("id"));
+        setUser(userData.data);
+      } else {
+        setUser(null);
+      }
+    })();
+  }, []);
+
   return (
     <div>
-      <Header />
+      <Header user={user} />
     </div>
   );
 };
